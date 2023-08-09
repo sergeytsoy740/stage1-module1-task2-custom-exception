5c2a90bfc652cc4aa183a7f90ac728c5a5e60e80
Sergey Tsoy <sergeytsoy740@gmail.com>
2023-08-09 23:00:01 +0600

    Stage-1 Task 2

---
+++ src/main/java/com/epam/mjc/IdNotExistsException.java
@@ -0,0 +1,12 @@
+package com.epam.mjc;
+
+public class IdNotExistsException extends IllegalArgumentException {
+    public IdNotExistsException(String message) {
+        super(message);
+    }
+
+    public IdNotExistsException() {
+
+    }
+
+}
--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -1,21 +1,41 @@
 package com.epam.mjc;
 
+/*   Task 1:
+ * Need create your custom exception which is inherited IllegalArgumentException.
+
+ *   Task 2:
+ * Your custom exception should be return custom message like "Could not find student with ID {id}".
+
+ *   Task 3:
+ * Implement method 'find' in class StudentManager which are throw your custom exception
+ * if this student is not found.
+ */
 
 public class StudentManager {
 
-  private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+    private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) {
-    return Student.getValueOf(studentID);
-  }
-
-  public static void main(String[] args) {
-    StudentManager manager = new StudentManager();
-
-    for (int i = 0; i < IDs.length; i++) {
-      Student student = manager.find(IDs[i]);
-      System.out.println("Student name " + student.getName());
+    public Student find(long studentID) throws IdNotExistsException {
+        Student s = Student.getValueOf(studentID);
+        try {
+            if (s != null) {
+                return s;
+            } else {
+                throw new NullPointerException();
+            }
+        } catch (NullPointerException e) {
+            //System.err.println(e.getMessage());
+            throw new IdNotExistsException("Could not find student with ID " + studentID);
+        }
     }
 
-  }
+    public static void main(String[] args) {
+        StudentManager manager = new StudentManager();
+
+        for (int i = 0; i < IDs.length; i++) {
+            Student student = manager.find(IDs[i]);
+            System.out.println("Student name " + student.getName());
+        }
+
+    }
 }
